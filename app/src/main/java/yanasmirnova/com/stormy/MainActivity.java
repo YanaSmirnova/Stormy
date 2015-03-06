0cb8dc6fff56d13bc92b55716221d29a608cbd0c
YanaSmirnova <smirnova.yana@gmail.com>
2015-03-06 22:38:26 +1100

    Handling the scenario where a network connection available

--- app/src/main/java/yanasmirnova/com/stormy/MainActivity.java
+++ app/src/main/java/yanasmirnova/com/stormy/MainActivity.java
@@ -1,10 +1,14 @@
 package yanasmirnova.com.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -25,41 +29,60 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         String apiKey = "5c868a1f47e78ac86297b36dcfde4609";
-        double latitude = 9999; //37.8267;
+        double latitude = 37.8267;
         double longitude = -122.423;
 
         String forecastUrl = "https://api.forecast.io/forecast/"+apiKey+"/"+latitude+","+longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(forecastUrl)
-                .build();
+        if (isNetworkAvailable()) {
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
 
-            }
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
-            @Override
-            public void onResponse(Response response) throws IOException {
-                try {
-                    Log.v(TAG, response.body().string());
-                    if (response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
-                    }
-                    else {
-                        alertUserAboutError();
-                    }
-                } catch (IOException e) {
-                    Log.i(TAG, "Exception caught", e);
                 }
-            }
-        });
 
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, response.body().string());
+                        }
+                        else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.i(TAG, "Exception caught", e);
+                    }
+                }
+            });
+        }
+        else {
+            // TODO make it a dialog
+            Toast.makeText(this, getString(R.string.network_unavailable_msg),
+                    Toast.LENGTH_LONG).show();
+        }
         Log.d(TAG, "Main UI code is running");
 
+    }
+
+    private boolean isNetworkAvailable() {
+        boolean isAvailable = false;
+
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return  isAvailable;
     }
 
     private void alertUserAboutError() {
