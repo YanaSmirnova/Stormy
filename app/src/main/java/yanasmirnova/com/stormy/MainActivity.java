6277fbf7e67fd1fb0247a1703713d175c7f7de68
YanaSmirnova <smirnova.yana@gmail.com>
2015-03-07 19:59:14 +1100

    Set a method to get data from Json file

--- app/src/main/java/yanasmirnova/com/stormy/MainActivity.java
+++ app/src/main/java/yanasmirnova/com/stormy/MainActivity.java
@@ -16,12 +16,17 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,14 +56,19 @@ public class MainActivity extends ActionBarActivity {
                 @Override
                 public void onResponse(Response response) throws IOException {
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-                            Log.v(TAG, response.body().string());
+                            mCurrentWeather = getCurrentDetails(jsonData);
                         }
                         else {
                             alertUserAboutError();
                         }
-                    } catch (IOException e) {
+                    }
+                    catch (IOException e) {
+                        Log.i(TAG, "Exception caught", e);
+                    }
+                    catch (JSONException e) {
                         Log.i(TAG, "Exception caught", e);
                     }
                 }
@@ -71,6 +81,13 @@ public class MainActivity extends ActionBarActivity {
         }
         Log.d(TAG, "Main UI code is running");
 
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timezone);
+        return null;
     }
 
     private boolean isNetworkAvailable() {
