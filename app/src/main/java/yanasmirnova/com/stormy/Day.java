eff76d252caa8ffb64afa74184e536d9fabeda97
YanaSmirnova <smirnova.yana@gmail.com>
2015-03-16 23:33:56 +1100

    Added a new class to hold daily data

---
+++ app/src/main/java/yanasmirnova/com/stormy/Day.java
@@ -0,0 +1,52 @@
+package yanasmirnova.com.stormy;
+
+/**
+ * Class to hold daily data
+ */
+public class Day {
+    private String mIcon;
+    private long mTime;
+    private double mTemperatureMax;
+    private String mSummary;
+    private String mTimeZone;
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperatureMax() {
+        return mTemperatureMax;
+    }
+
+    public void setTemperatureMax(double temperatureMax) {
+        mTemperatureMax = temperatureMax;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+}
