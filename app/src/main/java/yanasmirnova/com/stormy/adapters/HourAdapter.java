11a7cc55b5356d5b9e6716df44ab424076e7da4c
YanaSmirnova <smirnova.yana@gmail.com>
2015-04-01 22:16:23 +1100

    Added required methods to create a RecyclerView adapter and a custom constructor to use in our Activities

--- app/src/main/java/yanasmirnova/com/stormy/adapters/HourAdapter.java
+++ app/src/main/java/yanasmirnova/com/stormy/adapters/HourAdapter.java
@@ -2,10 +2,13 @@ package yanasmirnova.com.stormy.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 import yanasmirnova.com.stormy.R;
 import yanasmirnova.com.stormy.weather.Hour;
@@ -15,27 +18,28 @@ import yanasmirnova.com.stormy.weather.Hour;
  */
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
 
-    private Context mContext;
     private Hour[] mHours;
 
     public HourAdapter(Context context, Hour[] hours) {
-        mContext = context;
         mHours = hours;
     }
 
     @Override
     public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.hourly_list_item, parent, false);
+        HourViewHolder viewHolder = new HourViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(HourViewHolder holder, int position) {
-
+        holder.bindHour(mHours[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mHours.length;
     }
 
     public class HourViewHolder extends RecyclerView.ViewHolder {
