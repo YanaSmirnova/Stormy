32a42068bc92d86e21b60d49a30813f47901ccad
YanaSmirnova <smirnova.yana@gmail.com>
2015-04-01 21:18:06 +1100

    Created a new layout for the hourly forecast item details

--- app/src/main/java/yanasmirnova/com/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/yanasmirnova/com/stormy/ui/HourlyForecastActivity.java
@@ -5,17 +5,21 @@ import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.Arrays;
 
+import butterknife.InjectView;
 import yanasmirnova.com.stormy.R;
 import yanasmirnova.com.stormy.weather.Hour;
 
 public class HourlyForecastActivity extends ListActivity {
 
     private Hour[] mHours;
+    @InjectView(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,10 @@ public class HourlyForecastActivity extends ListActivity {
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
 
     }
 
